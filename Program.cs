6bf7589834ca0c43e6209739251f0224e687894d
ostiil <askrasenko@gmail.com>
2022-03-17 00:18:45 +0300

    task_07

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAS_Task_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n; 
+            Console.Write("Укажите количество элементов в массиве: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            double[] massiv = new double[n];
+            Random random = new Random();
+            for (int i = 0; i<n; i++)
+            {
+                massiv[i] = random.Next();
+            }
+            Console.WriteLine($"Изначальный массив: {string.Join("  ", massiv)}");
+            Shuffle(massiv);
+            Console.WriteLine($"Измененный массив: { string.Join("  ", massiv)}");
+            Console.ReadKey();
+        }
+        public static void Shuffle(double[] massiv)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                var index = rnd.Next(0, massiv.Count());
+                var index2 = rnd.Next(0, massiv.Count());
+                var temp = massiv[index];
+                massiv[index] = massiv[index2];
+                massiv[index2] = temp;
+            }
+        }
+    }
+
+}
+
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("KAS_Task_07")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KAS_Task_07")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5089d0b6-a47d-47a4-a1eb-e13eb8ca32be")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
